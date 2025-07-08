f9ac5296a6745040eae3fa1a17d607fe179c91af
leodagan13 <69777975+leodagan13@users.noreply.github.com>
2025-07-08 18:26:49 +0200

    Update agents configuration

--- .agents/index.ts
+++ .agents/index.ts
@@ -3,7 +3,7 @@ import { AgentConfig } from '@/types/agent';
 export const agents: AgentConfig[] = [
   {
     "id": "agent_1751851652568",
-    "name": "testnamer",
+    "name": "testname",
     "category": "testcategory",
     "role": "tester",
     "goal": "the prompt contains a number, write this exact amount of time the word \"test\"",
