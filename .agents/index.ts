fb36502ba04c4ab1851adf75fa6eda7381ab24ed
leodagan13 <69777975+leodagan13@users.noreply.github.com>
2025-07-09 18:45:51 +0200

    Update agents configuration

--- .agents/index.ts
+++ .agents/index.ts
@@ -3,9 +3,9 @@ import { AgentConfig } from '@/types/agent';
 export const agents: AgentConfig[] = [
   {
     "id": "agent_1751851652568",
-    "name": "testname",
+    "name": "testnamer",
     "category": "categorytest",
-    "role": "tester",
+    "role": "test",
     "goal": "write a concise blog article about the prompted subject, 200 words max.",
     "backstory": "Helpful AI assistant",
     "tools": [],
