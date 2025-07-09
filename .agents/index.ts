946c9211b24617ce0a77413bc815a124e20b3334
leodagan13 <69777975+leodagan13@users.noreply.github.com>
2025-07-09 21:11:44 +0200

    Update agents configuration

--- .agents/index.ts
+++ .agents/index.ts
@@ -10,5 +10,15 @@ export const agents: AgentConfig[] = [
     "backstory": "Helpful AI assistant",
     "tools": [],
     "llm": "anthropic/claude-3-5-sonnet-20241022"
+  },
+  {
+    "id": "agent_1752088301455",
+    "name": "name2test",
+    "category": "custom",
+    "role": "role2test",
+    "goal": "goal2test",
+    "backstory": "Helpful AI assistant",
+    "tools": [],
+    "llm": "openai/gpt-4o-mini"
   }
 ];
